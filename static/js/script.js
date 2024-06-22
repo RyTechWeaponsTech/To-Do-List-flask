4ca8b4c5a2dc3f8345eb67dc949cff2f4cf2fcc4
archivedghost001 <aryamuhammadkamil@gmail.com>
2024-06-22 10:49:21 +0700

    added .task-box outside of responsive in css, added something in js and html

--- static/js/script.js
+++ static/js/script.js
@@ -18,4 +18,59 @@ filters.forEach((btn) => {
 });
 
 // create function to add list in class (.task-box)
-function showTodo(filter) {}
+function showTodo(filter) {
+  let listTag = "";
+  if (todos) {
+    todos.forEach((todo, id) => {
+      let completed = todo.status == "completed" ? "checked" : "";
+      //logical OR operator (||) in JS
+      if (filter == todo.status || filter == "all") {
+        listTag += `<li class="task">
+          <label for="${id}">
+            <input
+              onclick="updateStatus(this)"
+              type="checkbox"
+              id="${id}"
+              ${completed}
+            />
+            <p class="${completed}">${todo.name}</p>
+          </label>
+          <div class="settings">
+            <i onclick="showMenu(this)" class="uil uil-ellipsis-h"></i>
+            <ul class="task-menu">
+              <li onclick='editTask(${id}, "${todo.name}")'>
+                <i class="uil uil-pen"></i>Edit
+              </li>
+              <li onclick='deleteTask(${id}, "${filter}")'>
+                <i class="uil uil-trash"></i>Delete
+              </li>
+            </ul>
+          </div>
+        </li>`;
+      }
+    });
+  }
+  // Set the innerHTML of taskBox to either listTag or message indicating no tasks
+  taskBox.innerHTML =
+    listTag || `<span>You don't have any task right now!</span>`;
+
+  // Check if there are any tasks present
+  let checkTask = taskBox.querySelectorAll(".task");
+  if (!checkTask.length) {
+    // If no tasks, remove active class from clearAll button
+    clearAll.classList.remove("active");
+  } else {
+    // If tasks present, add active class to clearAll button
+    clearAll.classList.add("active");
+  }
+  // Check if the height of taskBox is more than or equal to 265
+  if (taskBox.offsetHeight >= 265) {
+    // If height is more, add overflow class to taskBox
+    taskBox.classList.add("overflow");
+  } else {
+    // If height is less, remove overflow from taskBox
+    taskBox.classList.remove("overflow");
+  }
+}
+// to show all the lists
+showTodo("all");
