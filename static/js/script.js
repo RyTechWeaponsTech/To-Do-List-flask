31eb33b83f96aeaf682645b22927ada03ad18ee1
archivedghost001 <aryamuhammadkamil@gmail.com>
2024-06-29 21:22:38 +0700

    adding some codes regarding edit, delete, update status, pressing enter, and triple dots in JS and task box and settings in css

--- static/js/script.js
+++ static/js/script.js
@@ -11,7 +11,7 @@ let editId,
 // for connecting with ".filter span" property
 filters.forEach((btn) => {
   btn.addEventListener("click", () => {
-    document.querySelector("span.active").classList.add("active");
+    document.querySelector("span.active").classList.remove("active");
     btn.classList.add("active");
     showTodo(btn.id);
   });
@@ -74,3 +74,79 @@ function showTodo(filter) {
 }
 // to show all the lists
 showTodo("all");
+
+/* This event listener is triggered when 
+a key is released in the taskInput element */
+taskInput.addEventListener("keyup", (e) => {
+  let userTask = taskInput.value.trim();
+  if (e.key == "Enter" && userTask) {
+    if (!isEditTask) {
+      todos = !todos ? [] : todos;
+      let taskInfo = { name: userTask, status: "pending" };
+      todos.push(taskInfo);
+    } else {
+      isEditTask = false;
+      todos[editId].name = userTask;
+    }
+    taskInput.value = "";
+    localStorage.setItem("todo-list", JSON.stringify(todos));
+    showTodo(document.querySelector("span.active").id);
+  }
+});
+
+/* This event listener is triggered when the clearAll element is clicked 
+ It clears all the tasks in the todos array and updates the localStorage */
+clearAll.addEventListener("click", () => {
+  isEditTask = false;
+  todos.splice(0, todos.length);
+  localStorage.setItem("todo-list", JSON.stringify(todos));
+  showTodo();
+});
+
+/* showMenu is responsible for displaying 
+a menu associated with a selected task.*/
+function showMenu(selectedTask) {
+  let menuDiv = selectedTask.parentElement.lastElementChild;
+  menuDiv.classList.add("show");
+  document.addEventListener("click", (e) => {
+    if (e.target.tagName != "I" || e.target != selectedTask) {
+      menuDiv.classList.remove("show");
+    }
+  });
+}
+
+// This function updates the status of a selected task
+function updateStatus(selectedTask) {
+  let taskName = selectedTask.parentElement.lastElementChild;
+  // Check if there are any tasks present
+  if (selectedTask.checked) {
+    taskName.classList.add("checked");
+    todos[selectedTask.id].status = "completed";
+  } else {
+    taskName.classList.remove("checked");
+    todos[selectedTask.id].status = "pending";
+  }
+  // Update the localStorage with the updated "todos" array
+  localStorage.setItem("todo-list", JSON.stringify(todos));
+}
+
+/* This function is used to edit a task
+ It takes two parameters: taskId (the id of the task to be edited) 
+ and textName (the text content of the task) */
+function editTask(taskId, textName) {
+  editId = taskId;
+  isEditTask = true;
+  taskInput.value = textName;
+  taskInput.focus();
+  taskInput.classList.add("active");
+}
+
+/* This function is used to delete a task
+ It takes two parameters: deleteId (the id of the task to be deleted) 
+ and filter (the current filter value) */
+function deleteTask(deleteId, filter) {
+  isEditTask = false;
+  todos.splice(deleteId, 1);
+  localStorage.setItem("todo-list", JSON.stringify(todos));
+  showTodo(filter);
+}
